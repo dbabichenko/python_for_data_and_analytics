882b217e00b523f76c879b6c610bf1aab3b2e018
albuszheng <albuszheng@hotmail.com>
2025-09-25 15:16:47 -0400

    Clustering first class

--- Clustering/kmeans-sklearn.py
+++ Clustering/kmeans-sklearn.py
@@ -1,10 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
-plt.rcParams['figure.figsize'] = (16, 9)
+plt.rcParams["figure.figsize"] = (16, 9)
 
 # Creating a sample dataset with 4 clusters
 X, y = make_blobs(n_samples=800, n_features=3, centers=4)
@@ -24,4 +24,4 @@ C = kmeans.cluster_centers_
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
-ax.scatter(C[:, 0], C[:, 1], C[:, 2], marker='*', c='#050505', s=1000)
+ax.scatter(C[:, 0], C[:, 1], C[:, 2], marker="*", c="#050505", s=1000)
